364214b80b8cb6eb584ff1da7fabdd4429c854a0
kshamaj15 <kshamaj150896@gmail.com>
2021-07-26 15:41:47 +0530

    f commit

---
+++ Config/install.sh
@@ -0,0 +1,1 @@
+sudo cp ./mkqdir.sh /bin/mkqdir
---
+++ Config/mkqdir.sh
@@ -0,0 +1,8 @@
+dirName=$1
+
+mkdir $dirName/
+
+touch $dirName/input.txt
+touch $dirName/output.txt
+touch $dirName/$dirName.md
+touch $dirName/$dirName.cpp
