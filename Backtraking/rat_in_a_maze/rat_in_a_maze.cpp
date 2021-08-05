b00df38047cdae071062c3ddeaf87887c4f22784
kshamaj15 <kshamaj150896@gmail.com>
2021-08-05 09:28:00 +0530

    rat in a maze

--- Backtraking/rat_in_a_maze/rat_in_a_maze.cpp
+++ Backtraking/rat_in_a_maze/rat_in_a_maze.cpp
@@ -1,116 +1,49 @@
 // https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1
 
-// in progress
-
 #include <bits/stdc++.h>
 using namespace std;
 
 vector<string> fresult;
 
-void findAns(string ans, int dir, vector<vector<int>> &m, int n, int row, int col, vector<vector<bool>>& visited)
-{
-    // cout << row << " " << col << endl;
-    if (row == n - 1 && col == n - 1)
-    {
-        fresult.push_back(ans);
-        return;
-    }
-    bool isSafe = false;
+void findPath(int dir, int row, int col, int n, string ans, vector<vector<int>>&m, vector<vector<bool>>&visited) {
+    if(row < 0 || col < 0 || row >= n || col >= n)
+    return;
+    else if(!m[row][col])
+    return;
 
-    if (dir == 0)
-    {
-        ans += 'L';
-        row++;
-    }
-    else if (dir == 1)
-    {
-        ans += 'D';
-        col++;
-    }
-    else if (dir == 2)
-    {
-        ans += 'R';
-        row--;
-    }
-    else if (dir == 3)
-    {
-        ans += 'U';
-        col--;
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        int urow = row;
-        int ucol = col;
-        bool isSafe = false;
+    visited[row][col] = true;
 
-        // if (i == 0 && col < n - 1)
-        // {
-        //     isSafe = true;
-        //     ucol = col + 1;
-        // }
-        // else if (i == 1 && row < n - 1)
-        // {
-        //     isSafe = true;
-        //     urow = row + 1;
-        // }
-        // else if (i == 2 && col > 0)
-        // {
-        //     isSafe = true;
-        //     ucol = col - 1;
-        // }
-        // else if (i == 3 && row > 0)
-        // {
-        //     isSafe = true;
-        //     urow = row - 1;
-        // }
-        cout<<urow<<" "<<ucol<<endl;
-        if (m[urow][ucol] && !visited[urow][ucol]) 
-        {
-            visited[urow][ucol] = true;
-            findAns(ans, i, m, n, urow, ucol, visited);
-            visited[urow][ucol] = false;
+    if(row == n-1 && col == n-1){
+        // cout<<"ans "<<ans<<endl;
+        if(!fresult.size() || fresult[fresult.size() - 1] != ans)
+            fresult.push_back(ans);
+            return;
+    }
+    
+    switch (dir)
+    {
+        case 0: ans += 'R'; col++; break;
+        case 1: ans += 'D'; row++; break;
+        case 2: ans += 'L'; col--; break;       
+        case 3: ans += 'U'; row--; break;
+    }
+
+    if(row >= 0 && col >= 0 && row < n && col < n) {
+        if(!m[row][col] && visited[row][col]) ans.pop_back();
+        if(!visited[row][col]) {
+        for (int i = 0; i < 4; i++) {
+            findPath(i, row, col, n, ans, m, visited);
+        }
+        visited[row][col] = false;
         }
     }
-    cout << "ans " << ans << endl;
-    if (ans.size())
-        ans.pop_back();
-
-
-    if (dir == 0)
-    {
-        row--;
-    }
-    else if (dir == 1)
-    {
-        col--;
-    }
-    else if (dir == 2)
-    {
-        row++;
-    }
-    else if (dir == 3)
-    {
-        col++;
-    }
-}
-
-vector<string> findPath(vector<vector<int>> &m, int n)
-{
-    vector<vector<bool>> visited(n, vector<bool>(n, false));
-    string ans = "";
-    if (m[0][0] && m[0][1])
-        findAns(ans, 0, m, n, 0, 0, visited);
-    // ans = "D";
-    if (m[0][0] && m[1][0])
-        findAns(ans, 1, m, n, 0, 0, visited);
-    return fresult;
 }
 
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    freopen("outpu.txt", "w", stdout);
 #endif
 
     int t;
@@ -121,6 +54,7 @@ int main()
         int n;
         cin >> n;
         vector<vector<int>> m(n, vector<int>(n, 0));
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
@@ -129,18 +63,26 @@ int main()
             }
         }
 
-        vector<string> result = findPath(m, n);
+        // m = {{1,0}, {1,1}};
 
-        if (result.size())
+        string ans = "";
+
+       findPath(0,0,0, n, ans, m, visited);
+       findPath(1,0,0, n, ans, m, visited);
+
+        if (fresult.size())
         {
             for (int i = 0; i < n; i++)
             {
                 cout << fresult[i] << endl;
             }
+            cout<<endl;
         }
         else
         {
             cout << "No result found" << endl;
         }
+
     }
+
 }
